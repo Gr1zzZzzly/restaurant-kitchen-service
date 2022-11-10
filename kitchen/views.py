118556cff56318de0624783479d94448199b4f87
igor kosianenko <funkcionalkos@gmail.com>
2022-11-10 17:22:02 +0200

    add base.html and index.html, add ursl for this files

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Cook, Dish, DishType
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    count_cooks = Cook.objects.count()
+    count_dishes = Dish.objects.count()
+    count_dichtipes = DishType.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "count_cooks": count_cooks,
+        "count_dishes": count_dishes,
+        "count_dichtipes": count_dichtipes,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
